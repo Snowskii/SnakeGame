17e54972fe35f6948e44702a893cf5012a0f9830
Snowskii <kiralmichal1@gmail.com>
2024-06-07 17:46:19 +0200

    defining main

--- main.py
+++ main.py
@@ -3,50 +3,56 @@ from game import Game
 from color import Color
 from commands import Commands
 
-game = Game()
 
-# Main Function
-while True:
+def main():
+    game = Game()
 
-    output = game.handle_events(game.snake.direction)
-    if output != Commands.QUIT:
-        game.snake.move_snake(output)
-        game.snake.direction = output
-    else:
-        game.game_over()
+    # Main Function
+    while True:
 
-    game.snake.body.insert(0, list(game.snake.position))
-    if game.snake.position[0] == game.apple.position[0] and \
-            game.snake.position[1] == game.apple.position[1]:
-        game.score += 1
-        game.apple.spawn = False
-    else:
-        game.snake.body.pop()
-
-    if not game.apple.spawn:
-        game.apple.set_random_pos()
-        game.apple.spawn = True
-
-    game.window.fill(Color.BLACK)
-
-    for pos in game.snake.body:
-        pygame.draw.rect(game.window, Color.GREEN,
-                         pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(game.window, Color.WHITE, pygame.Rect(
-        game.apple.position[0], game.apple.position[1], 10, 10))
-
-    # Game Over conditions
-    if game.snake.position[0] < 0 or game.snake.position[0] > Game.window_x-10:
-        game.game_over()
-    if game.snake.position[1] < 0 or game.snake.position[1] > Game.window_y-10:
-        game.game_over()
-
-    for block in game.snake.body[1:]:
-        if game.snake.position[0] == block[0] and game.snake.position[1] == block[1]:
+        output = game.handle_events(game.snake.direction)
+        if output != Commands.QUIT:
+            game.snake.move_snake(output)
+            game.snake.direction = output
+        else:
             game.game_over()
 
-    game.show_score(Color.WHITE, 'times new roman', 20, game.window)
+        game.snake.body.insert(0, list(game.snake.position))
+        if game.snake.position[0] == game.apple.position[0] and \
+                game.snake.position[1] == game.apple.position[1]:
+            game.score += 1
+            game.apple.spawn = False
+        else:
+            game.snake.body.pop()
 
-    pygame.display.update()
+        if not game.apple.spawn:
+            game.apple.set_random_pos()
+            game.apple.spawn = True
 
-    game.fps.tick(game.snake_speed)
+        game.window.fill(Color.BLACK)
+
+        for pos in game.snake.body:
+            pygame.draw.rect(game.window, Color.GREEN,
+                             pygame.Rect(pos[0], pos[1], 10, 10))
+        pygame.draw.rect(game.window, Color.WHITE, pygame.Rect(
+            game.apple.position[0], game.apple.position[1], 10, 10))
+
+        # Game Over conditions
+        if game.snake.position[0] < 0 or game.snake.position[0] > Game.window_x-10:
+            game.game_over()
+        if game.snake.position[1] < 0 or game.snake.position[1] > Game.window_y-10:
+            game.game_over()
+
+        for block in game.snake.body[1:]:
+            if game.snake.position[0] == block[0] and game.snake.position[1] == block[1]:
+                game.game_over()
+
+        game.show_score(Color.WHITE, 'times new roman', 20, game.window)
+
+        pygame.display.update()
+
+        game.fps.tick(game.snake_speed)
+
+
+if __name__ == "__main__":
+    main()

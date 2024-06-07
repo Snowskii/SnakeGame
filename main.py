4da2d296179b32d01fd7bb2f8691c6b0080fc145
Snowskii <kiralmichal1@gmail.com>
2024-06-07 15:53:09 +0200

    Game over and show Score implemented

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 import time
 import random
+from score import Score
 
-snake_speed = 15
 
-window_x = 720
-window_y = 480
+class Game():
+    snake_speed: int = 15
+    window_x: int = 720
+    window_y: int = 480
+    score: int = 0
 
 
 class Color():
@@ -15,8 +18,50 @@ class Color():
     GREEN = pygame.Color(0, 255, 0)
     BLUE = pygame.Color(0, 0, 255)
 
+
 # Initialization of game
 pygame.init()
 pygame.display.set_caption("SnakeGame with a twist?")
-game_window = pygame.display.set_mode((window_x, window_y))
+game_window = pygame.display.set_mode((Game.window_x, Game.window_y))
+fps = pygame.time.Clock()
 
+snake_position = [100, 50]
+snake_body = [
+    [100, 50]
+    [90, 50]
+    [80, 50]
+    [70, 50]
+]
+
+apple_position = [random.randrange(1, (Game.window_x//10)) * 10,
+                  random.randrange(1, (Game.window_y//10)) * 10]
+apple_spawn = True
+
+direction = "RIGHT"
+change_to = direction
+
+
+def show_score(choice, color, font, size, game_window):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(Game.score), True,
+                                      color)
+    score_rect = score_surface.get_rect()
+    game_window.blit(score_surface, score_rect)
+
+
+# game over function
+def game_over():
+
+    my_font = pygame.font.SysFont('times new roman', 50)
+    game_over_surface = my_font.render('Your Score is : ' + str(Game.score),
+                                       True, Color.RED)
+
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (Game.window_x/2, Game.window_y/4)
+
+    game_window.blit(game_over_surface, game_over_rect)
+
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    quit()
